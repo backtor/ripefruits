c74e7a549b565db8d3a6feef67a61e48162d1176
Dave Watson <david.watson@zen.co.uk>
2016-01-24 13:44:39 +0000

    First go at running app. Gives minimised JSON.

--- src/main/java/backtor/App.java
+++ src/main/java/backtor/App.java
@@ -1,13 +1,20 @@
 package backtor;
 
+import java.net.URL;
+
+import backtor.grocery.adapter.JsonAdapter;
+
 /**
- * Hello world!
- *
+ * Adaptor to allow console operation of the hexagon created by the {@link AppFactory}.
+ * @author Dave Watson
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+    	
+    	URL sourceUrl = new URL("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html");
+        JsonAdapter adapter = AppFactory.createJsonHtmlScraper(sourceUrl);
+        
+        String json = adapter.fetchProductsAsJson();
+    	System.out.println(json);
     }
 }
