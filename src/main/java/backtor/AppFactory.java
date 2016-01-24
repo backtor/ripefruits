67277cf69ded8854cd42bf24c4f40c364614df79
Dave Watson <david.watson@zen.co.uk>
2016-01-24 13:32:39 +0000

    Added IoC factory to bind application together.

---
+++ src/main/java/backtor/AppFactory.java
@@ -0,0 +1,31 @@
+package backtor;
+
+import java.net.URL;
+
+import backtor.grocery.adapter.JsonAdapter;
+import backtor.grocery.adapter.ProductFetcherHTMLScraperDAO;
+import backtor.grocery.service.api.ProductFetcher;
+import backtor.grocery.service.api.ProductFetcherDAO;
+import backtor.grocery.service.impl.ProductFetcherServiceImpl;
+
+/**
+ * IoC container that binds the application together. Could use spring for bigger projects.
+ */
+public class AppFactory {
+
+	private AppFactory() {
+	}
+	
+	/**
+	 * Return application 'hexagon' (In hexagonal architecture nomenclature) to scrape HTML at given URL and return product information in JSON.
+	 * @param sourceUrl The given URL.
+	 * @return Application to retrieve JSON.
+	 */
+	public static JsonAdapter createJsonHtmlScraper(URL sourceUrl) {
+		ProductFetcherDAO daoAdapter = new ProductFetcherHTMLScraperDAO(sourceUrl);
+		ProductFetcher service = new ProductFetcherServiceImpl(daoAdapter);
+		JsonAdapter jsonAdapter = new JsonAdapter(service);
+		
+		return jsonAdapter;
+	}
+}
